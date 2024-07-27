40914646147da50335ab1d07ffff5f1134eb1fc6
PhuongFX <162846217+PhuongFX@users.noreply.github.com>
2024-07-27 23:58:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -106,9 +106,9 @@ with gr.Blocks() as demo:
      gr.Examples(
             examples=[
                 "Speech_samples/consumer4.wav", 
-                "Speech_samples/samples_audio-files_05-gettysburg-address-2min.wav"
-                "Speech_samples/samples_audio-files_12-jfk-speech-12sec.wav"
-                "Speech_samples/harvard.wav"
+                "Speech_samples/samples_audio-files_05-gettysburg-address-2min.wav",
+                "Speech_samples/samples_audio-files_12-jfk-speech-12sec.wav",
+                "Speech_samples/harvard_long.wav",
             ],
             inputs=audio_input,
             label="Try these examples")
